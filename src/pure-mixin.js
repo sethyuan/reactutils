91641cfafc54ed309d1c264d19db06873c9ea437
sethyuan <sethyuan@gmail.com>
2016-09-04 21:16:56 +0800

    fix: pure mixin typo

--- src/pure-mixin.js
+++ src/pure-mixin.js
@@ -3,7 +3,7 @@ import {displayName} from "./utils"
 
 export default function pure(Comp) {
   return class PureComp extends Comp {
-    static displayName = `${displayName(Comp)-Pure}`
+    static displayName = `${displayName(Comp)}-Pure`
 
     shouldComponentUpdate(nextProps, nextState) {
       return shallowCompare(this, nextProps, nextState)
