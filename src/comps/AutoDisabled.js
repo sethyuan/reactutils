52619178fbbf60de06b06fc12072a99627edcaf8
seth <seth@teamyi.com>
2019-05-17 17:13:22 +0800

    fix: auto disabled logic

--- src/comps/AutoDisabled.js
+++ src/comps/AutoDisabled.js
@@ -4,29 +4,15 @@ export default function AutoDisabled(props) {
   const [state, setState] = useState({
     disabled: false,
     duringClick: false,
-    args: null,
   })
   const mountedRef = useRef(false)
 
   useEffect(() => {
     mountedRef.current = true
-
-    if (state.duringClick && props.onClick) {
-      ;(async () => {
-        try {
-          await props.onClick(state.args)
-        } finally {
-          if (mountedRef.current) {
-            setState({ disabled: false, duringClick: false, args: null })
-          }
-        }
-      })()
-    }
-
     return () => {
       mountedRef.current = false
     }
-  })
+  }, [])
 
   const disabled =
     props.disabled === undefined ? state.disabled : props.disabled
@@ -34,9 +20,21 @@ export default function AutoDisabled(props) {
   const duringClick =
     props.duringClick === undefined ? state.duringClick : props.duringClick
 
-  const onClick = useCallback((...args) => {
-    setState({ disabled: true, duringClick: true, args })
-  }, [])
+  const onClick = useCallback(
+    async (...args) => {
+      if (props.onClick) {
+        setState({ disabled: true, duringClick: true })
+        try {
+          await props.onClick(...args)
+        } finally {
+          if (mountedRef.current) {
+            setState({ disabled: false, duringClick: false })
+          }
+        }
+      }
+    },
+    [props.onClick],
+  )
 
   return props.children(disabled, onClick, duringClick)
 }
