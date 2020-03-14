bb46a222814d628c399c28fd4374fa890aa103f1
seth <seth@teamyi.com>
2020-03-14 16:33:38 +0800

    feat: add refetch to useMeta

--- src/hooks/useMeta.ts
+++ src/hooks/useMeta.ts
@@ -1,6 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 
 export type UseMetaType<T> = {
+  loading: boolean
+  data?: T
+  error?: { [key: string]: any }
+  refetch: () => void
+}
+
+type State<T> = {
   loading: boolean
   data?: T
   error?: { [key: string]: any }
@@ -10,23 +17,25 @@ export function useMeta<T>(
   fn: () => Promise<T>,
   deps?: React.DependencyList,
 ): UseMetaType<T> {
-  const [{ loading, data, error }, setState] = useState<UseMetaType<T>>({
+  const [{ loading, data, error }, setState] = useState<State<T>>({
     loading: true,
     data: undefined,
     error: undefined,
   })
 
+  const refetch = useCallback(async () => {
+    setState({ loading: true, data, error })
+    try {
+      const data = await fn()
+      setState({ loading: false, data, error: undefined })
+    } catch (err) {
+      setState({ loading: false, data: undefined, error: err })
+    }
+  }, [fn])
+
   useEffect(() => {
-    ;(async () => {
-      setState({ loading: true, data, error })
-      try {
-        const data = await fn()
-        setState({ loading: false, data, error: undefined })
-      } catch (err) {
-        setState({ loading: false, data: undefined, error: err })
-      }
-    })()
+    refetch()
   }, deps)
 
-  return { loading, data, error }
+  return { loading, data, error, refetch }
 }
