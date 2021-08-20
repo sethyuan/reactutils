c96bbbecad2b93f9ef10f74c51d197d72fde3eb0
seth <sethyuan@gmail.com>
2021-08-20 09:18:50 +0800

    feat: improve KeepPrevView

--- src/comps/KeepPrevView.tsx
+++ src/comps/KeepPrevView.tsx
@@ -1,19 +1,27 @@
-import React, { ComponentType, memo } from "react"
+import React, { ComponentType, Key, memo, useEffect } from "react"
 import { usePrev } from "../hooks/usePrev"
 
-type Props = {
+export type Props = {
   id: any
-  component: ComponentType
+  component: ComponentType<any>
+  onRestore?: (key: Key) => void
   [key: string]: any
 }
 
 export const KeepPrevView = memo(
-  ({ id, component, ...others }: Props) => {
+  ({ id, component, onRestore, ...others }: Props) => {
     const Comp = component
-    const jsx = <Comp {...others} />
+    const jsx = <Comp onRestore={onRestore} {...others} />
 
     const prevId = usePrev(id)
+    const prevPrevId = usePrev(prevId)
     const prevJsx = usePrev(jsx)
+
+    useEffect(() => {
+      if (id === prevPrevId) {
+        onRestore?.(id)
+      }
+    }, [id, prevPrevId])
 
     return (
       <>
