765780fad86bf83fb0211cc6b9a416c8c9bebaa4
seth <sethyuan@gmail.com>
2021-08-25 14:54:03 +0800

    feat: improve KeepPrevView to support custom scroll restoration

--- src/comps/KeepPrevView.tsx
+++ src/comps/KeepPrevView.tsx
@@ -4,28 +4,41 @@ import React, {
   Key,
   memo,
   ReactElement,
+  useLayoutEffect,
+  useRef,
 } from "react"
 import { usePrev } from "../hooks/usePrev"
 
 export type Props = {
-  kpvid: Key
-  component?: ComponentType<any>
-  render?: (props: { [key: string]: any }) => ReactElement
+  kpvId: Key
+  kpvComponent?: ComponentType<any>
+  kpvRender?: (props: { [key: string]: any }) => ReactElement
   [key: string]: any
 }
 
 export const KeepPrevView = memo(
-  ({ kpvid, component, render, ...others }: Props) => {
-    const prevId = usePrev(kpvid)
+  ({ kpvId, kpvComponent, kpvRender, ...others }: Props) => {
+    const prevId = usePrev(kpvId)
     const prevPrevId = usePrev(prevId)
 
-    const Comp = component!
-    const jsx = render ? (
-      render({ restored: kpvid === prevPrevId, ...others })
-    ) : (
-      <Comp restored={kpvid === prevPrevId} {...others} />
-    )
+    const restored = kpvId === prevPrevId
 
+    const scrollElementRef = useScrollRestore(restored)
+
+    const Comp = kpvComponent!
+    const jsx = kpvRender ? (
+      kpvRender({
+        kpvRestored: restored,
+        kpvScrollElementRef: scrollElementRef,
+        ...others,
+      })
+    ) : (
+      <Comp
+        kpvRestored={restored}
+        kpvScrollElementRef={scrollElementRef}
+        {...others}
+      />
+    )
     const prevJsx = usePrev(jsx)
 
     return (
@@ -35,11 +48,27 @@ export const KeepPrevView = memo(
             ? prevJsx
             : cloneElement(prevJsx, { restored: false })}
         </div>
-        <div key={kpvid}>{jsx}</div>
+        <div key={kpvId}>{jsx}</div>
       </>
     )
   },
-  (prev, next) => prev.kpvid === next.kpvid,
+  (prev, next) => prev.kpvId === next.kpvId,
 )
 
 KeepPrevView.displayName = "KeepPrevView"
+
+function useScrollRestore(restored: boolean) {
+  const scrollElementRef = useRef<HTMLElement>()
+  const prevScrollElement = usePrev(scrollElementRef.current)
+  const scrollTopRef = useRef<number>()
+  scrollTopRef.current = scrollElementRef.current?.scrollTop
+  const prevScrollTop = usePrev(scrollTopRef.current)
+
+  useLayoutEffect(() => {
+    if (restored && prevScrollElement != null && prevScrollTop != null) {
+      prevScrollElement.scrollTop = prevScrollTop
+    }
+  })
+
+  return scrollElementRef
+}
