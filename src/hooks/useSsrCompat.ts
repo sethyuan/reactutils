b81fa5fafcd9b410f82d3b834d3b0494d3daa530
seth <sethyuan@gmail.com>
2021-09-06 16:54:59 +0800

    feat: add ssr compat hook

---
+++ src/hooks/useSsrCompat.ts
@@ -0,0 +1,14 @@
+import { useCallback, useEffect, useState } from "react"
+
+export default function useSsrCompat() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return useCallback(
+    (render: () => any) => (mounted ? render() : null),
+    [mounted],
+  )
+}
