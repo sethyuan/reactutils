a1fe80f61bba20025388894f58469635e3310734
seth <sethyuan@gmail.com>
2021-09-07 13:20:56 +0800

    feat: improve ssr hook

--- src/hooks/useSsrCompat.ts
+++ src/hooks/useSsrCompat.ts
@@ -1,14 +1,16 @@
 import { useCallback, useEffect, useState } from "react"
+import { useDependentState } from "./useDependentState"
 
 export default function useSsrCompat() {
   const [mounted, setMounted] = useState(false)
+  const [rendered] = useDependentState(() => mounted, [mounted])
 
   useEffect(() => {
     setMounted(true)
   }, [])
 
-  return useCallback(
-    (render: () => any) => (mounted ? render() : null),
-    [mounted],
-  )
+  return [
+    useCallback((render: () => any) => (mounted ? render() : null), [mounted]),
+    rendered,
+  ] as const
 }
