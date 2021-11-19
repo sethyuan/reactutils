54ad7b5d6cf0c80e5069fd7a684dd139f2d36da2
Seth <sethyuan@gmail.com>
2021-11-19 10:40:37 +0800

    feat: add initial state to usedependentstate

--- src/hooks/useDependentState.ts
+++ src/hooks/useDependentState.ts
@@ -1,7 +1,11 @@
 import { DependencyList, useEffect, useState } from "react"
 
-export function useDependentState<T>(updater: () => T, deps: DependencyList) {
-  const [value, setValue] = useState(updater)
+export function useDependentState<T>(
+  updater: () => T,
+  deps: DependencyList,
+  initialState?: T,
+) {
+  const [value, setValue] = useState(initialState ?? updater)
 
   useEffect(() => setValue(updater()), deps)
 
